558a3edd2dd8f979f51c7bdd824a3d749f5664f9
maureennc <maurrueche@gmail.com>
2024-08-12 16:46:59 -0400

    add tbi scripts

--- lukens-lab/jess/AD-sting-project/global-ko-analysis.py
+++ lukens-lab/jess/AD-sting-project/global-ko-analysis.py
@@ -15,24 +15,29 @@ import matplotlib.ticker as ticker
 
 plt.rcParams['font.family'] = 'Arial'
 import seaborn as sns
-
+import scrublet as scr
 import scanpy as sc
+import scanpy.external as sce
 sc.set_figure_params(scanpy = True, dpi = 300, dpi_save = 400)
 from scipy.sparse import csr_matrix
 import scipy
-import squidpy as sq
-import scrublet as scr
 from scipy.sparse import csr_matrix
-
+#import scvi
 # environment = 'sc'
+
+
+import matplotlib
+#matplotlib.use('Qt5Agg')
+import matplotlib.pyplot as plt
+
 
 os.chdir("/Users/maureen/Documents/projects/lukens-lab/jess/Thanos_AD-Sting_snRNAseq")
 
 # Global KO
 
 ## Both files are in the KO directory
-gdata1 = sc.read_10x_mtx("1_5xSting_Global-KO/Sample2_5xSting_globalKO/1/filtered_feature_bc_matrix", var_names='gene_symbols', make_unique=True)
-gdata2 = sc.read_10x_mtx("1_5xSting_Global-KO/Sample2_5xSting_globalKO/2/filtered_feature_bc_matrix", var_names='gene_symbols', make_unique=True)
+adata1 = sc.read_10x_mtx("1_5xSting_Global-KO/Sample2_5xSting_globalKO/1/filtered_feature_bc_matrix", var_names='gene_symbols', make_unique=True)
+adata2 = sc.read_10x_mtx("1_5xSting_Global-KO/Sample2_5xSting_globalKO/2/filtered_feature_bc_matrix", var_names='gene_symbols', make_unique=True)
 
 
 
@@ -40,15 +45,15 @@ gdata2 = sc.read_10x_mtx("1_5xSting_Global-KO/Sample2_5xSting_globalKO/2/filtere
 
 # BASIC FILTERING
 
-sc.pp.filter_genes(gdata1, min_cells=3)
-sc.pp.filter_cells(gdata1, min_genes=200)
-gdata1.var['mt'] = gdata1.var_names.str.startswith('mt-')
-sc.pp.calculate_qc_metrics(gdata1, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
+sc.pp.filter_genes(adata1, min_cells=3)
+sc.pp.filter_cells(adata1, min_genes=200)
+adata1.var['mt'] = adata1.var_names.str.startswith('mt-')
+sc.pp.calculate_qc_metrics(adata1, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
 
-sc.pp.filter_genes(gdata2, min_cells=3)
-sc.pp.filter_cells(gdata2, min_genes=200)
-gdata2.var['mt'] = gdata2.var_names.str.startswith('mt-')
-sc.pp.calculate_qc_metrics(gdata2, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
+sc.pp.filter_genes(adata2, min_cells=3)
+sc.pp.filter_cells(adata2, min_genes=200)
+adata2.var['mt'] = adata2.var_names.str.startswith('mt-')
+sc.pp.calculate_qc_metrics(adata2, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
 
 ## Already successfully filtered by 10x Cellranger
 
@@ -91,7 +96,7 @@ def run_scrublet(adata_list):
     return scrublet_df
 
 
-adata_list = [gdata1, gdata2]
+adata_list = [adata1, adata2]
 scrublet_result = run_scrublet(adata_list)
 
 def filter_doublets(adata_list):
@@ -104,14 +109,14 @@ filter_doublets(adata_list)
 
 # QC DATA VISUALIZATION
 
-qc_metrics = ['total_counts', 'pct_counts_mt', 'total_counts_mt', 'n_genes', 'n_genes_by_counts']
+qc_metrics = ['total_counts', 'n_genes', 'n_genes_by_counts', 'total_counts_mt', 'pct_counts_mt' ]
 
 custom_titles = {
     'total_counts': 'Total Counts Distribution',
-    'pct_counts_mt': 'Mitochondrial Counts Percentage',
-    'total_counts_mt': 'Total Mitochondrial Counts',
     'n_genes': 'Number of Genes',
-    'n_genes_by_counts': 'Genes by Counts'
+    'n_genes_by_counts': 'Genes by Counts',
+    'total_counts_mt': 'Total Mitochondrial Counts',
+    'pct_counts_mt': 'Mitochondrial Counts Percentage'
 }
 
 # Loop for QC visualizations
@@ -121,8 +126,8 @@ for i, metric in enumerate(qc_metrics, 1): #Matplotlib's subplot indexing starts
     plt.subplot(2, 3, i)
     
     # Prepare the data
-    data1 = gdata1.obs[metric]
-    data2 = gdata2.obs[metric]
+    data1 = adata1.obs[metric]
+    data2 = adata2.obs[metric]
 
     combined_df = pd.concat([
         pd.DataFrame({metric: data1, 'Dataset': 'WT'}),
@@ -130,14 +135,15 @@ for i, metric in enumerate(qc_metrics, 1): #Matplotlib's subplot indexing starts
     ])
 
     sns.violinplot(x='Dataset', y=metric, data=combined_df)
-
+    sns.stripplot(x='Dataset', y=metric, data=combined_df, color='k', size = 1)
+    
     ax = plt.gca()
     ax.yaxis.set_major_locator(ticker.AutoLocator())
     ax.yaxis.set_minor_locator(ticker.AutoMinorLocator())
 
     plt.title(custom_titles[metric])
 
-plt.suptitle('QC: Global STING KO')
+plt.suptitle('QC: Global STING KO', y = 0.95) # decreasing y value moves title down
 plt.tight_layout(rect=[0, 0.03, 1, 0.95])
 plt.show()
 
@@ -145,53 +151,313 @@ plt.show()
 
 # MITOCHONDRIAL FILTER
 
-gdata1 = gdata1[gdata1.obs.pct_counts_mt < 5, :].copy()
-gdata2 = gdata2[gdata2.obs.pct_counts_mt < 5, :].copy()
+adata1 = adata1[adata1.obs.pct_counts_mt < 5, :].copy()
+adata2 = adata2[adata2.obs.pct_counts_mt < 5, :].copy()
 
 ################################################################################################################################
 
 # PRE-PROCESSING
 
 ## Normalization and Log1p transformation
-gdata1.layers['counts'] = gdata1.X.copy()
-sc.pp.normalize_total(gdata1, target_sum = 1e4)
-gdata1.layers['normalized'] = gdata1.X.copy()
-sc.pp.log1p(gdata1)
-gdata1.layers['log1p'] = gdata1.X.copy()
-gdata1.raw = gdata1
+adata1.layers['counts'] = adata1.X.copy()
+sc.pp.normalize_total(adata1, target_sum = 1e4)
+adata1.layers['normalized'] = adata1.X.copy()
+sc.pp.log1p(adata1)
+adata1.layers['log1p'] = adata1.X.copy()
+adata1.raw = adata1
 
 
-gdata2.layers['counts'] = gdata2.X.copy()
-sc.pp.normalize_total(gdata2, target_sum = 1e4)
-gdata2.layers['normalized'] = gdata2.X.copy()
-sc.pp.log1p(gdata2)
-gdata2.layers['log1p'] = gdata2.X.copy()
-gdata2.raw = gdata2
+adata2.layers['counts'] = adata2.X.copy()
+sc.pp.normalize_total(adata2, target_sum = 1e4)
+adata2.layers['normalized'] = adata2.X.copy()
+sc.pp.log1p(adata2)
+adata2.layers['log1p'] = adata2.X.copy()
+adata2.raw = adata2
 
+
+########
+
+
+## Set up and train model
+scvi.model.SCVI.setup_anndata(
+    adata1,
+    layer='counts',
+    #categorical_covariate_keys=[''],
+    continuous_covariate_keys=['total_counts'])
+
+model = scvi.model.SCVI(adata1)
+model.train()
+
+## Save model
+save_dir = ('./scVI')
+os.makedirs(save_dir, exist_ok=True)
+model_dir = os.path.join(save_dir, "scvi_model_2024-01-11_cosmx")
+model.save(model_dir, overwrite=True)
+
+## Extract model inputs
+SCVI_LATENT_KEY = "X_scVI"
+
+latent = model.get_latent_representation()
+adata.obsm[SCVI_LATENT_KEY] = latent
+latent.shape
+
+#####
 
 ### You can get back an AnnData of the object in .raw by calling .raw.to_adata()
 
-## Regress out effects of confounding variables
-sc.pp.regress_out(gdata1, ['total_counts', 'pct_counts_mt'])
-sc.pp.regress_out(gdata2, ['total_counts', 'pct_counts_mt'])
+## Regress out effects of confounding covariates
+sc.pp.regress_out(adata1, ['total_counts'])
+sc.pp.regress_out(adata2, ['total_counts'])
 
 ## Select HVGs
-sc.pp.highly_variable_genes(gdata1, n_top_genes=3000, subset=True, layer='counts', flavor="seurat_v3")
-sc.pp.highly_variable_genes(gdata2, n_top_genes=3000, subset=True, layer='counts', flavor="seurat_v3")
-
+sc.pp.highly_variable_genes(adata1, n_top_genes=3000, subset=True, layer='counts', flavor="seurat_v3")
+sc.pp.highly_variable_genes(adata2, n_top_genes=3000, subset=True, layer='counts', flavor="seurat_v3")
 
 ## Scale datasets
-sc.pp.scale(gdata1, max_value=10)
-sc.pp.scale(gdata2, max_value=10)
+sc.pp.scale(adata1, max_value=10)
+sc.pp.scale(adata2, max_value=10)
+
+
+################################################################################################################################
+
+# PCA & CLUSTERING
+
+sc.tl.pca(adata1, n_comps = 30)
+sc.pl.pca_variance_ratio(adata1, log=True, n_pcs=30)
+sc.pp.neighbors(adata1, n_neighbors=10, n_pcs=30)
+sc.tl.umap(adata1)
+sc.tl.leiden(adata1, resolution=0.2)
+
+
+sc.tl.pca(adata2, n_comps = 30)
+sc.pl.pca_variance_ratio(adata2, log=True, n_pcs=30)
+sc.pp.neighbors(adata2, n_neighbors=10, n_pcs=30)
+sc.tl.umap(adata2)
+sc.tl.leiden(adata2, resolution=0.2)
+
+
+################################################################################################################################
+
+# CONTROL - MARKERS
+
+sc.tl.rank_genes_groups(adata1, groupby='leiden', use_raw = True, method = 'wilcoxon')
+sc.pl.rank_genes_groups(adata1, n_genes=30)
+
+sc.pl.umap(adata1, color = ['Rbfox3', 'Gad1', 'Slc17a6', 'Snap25',
+                            'Mbp', 'Plp1', 'Sox10', 'Pdgfra', 'Ptprz1',
+                            'Slc1a2', 'Apoe', 'Atp1a2', 'Aqp4', 'Aldh1l1', 'Elmo1',
+                            'Hexb', 'Tgfbr1', 'C1qa',
+                            'Cldn5', 'Flt1', 'Vtn',
+                            'mt-Atp6', 'mt-Co1', 'Rpl13', 'Ptgds', 'Hsp90ab1', 
+                            'Pecam1', 'Rgs5'])
+
+
+markers1 = sc.get.rank_genes_groups_df(adata1, group = None)
+markers1 = markers1[(markers1.pvals_adj < 0.05) & (markers1.logfoldchanges > .5)]
+markers1
+
+
+markers1.loc[markers1['group'] == '0'].head(20)
+markers1.loc[markers1['group'] == '1'].head(20)
+markers1.loc[markers1['group'] == '2'].head(20)
+markers1.loc[markers1['group'] == '3'].head(20)
+markers1.loc[markers1['group'] == '4'].head(20)
+markers1.loc[markers1['group'] == '5'].head(20)
+markers1.loc[markers1['group'] == '6'].head(30)
+markers1.loc[markers1['group'] == '7'].head(20)
+markers1.loc[markers1['group'] == '8'].head(20)
+markers1.loc[markers1['group'] == '9'].head(20)
+markers1.loc[markers1['group'] == '10'].head(30)
+markers1.loc[markers1['group'] == '11'].head(20)
+markers1.loc[markers1['group'] == '12'].head(20)
+markers1.loc[markers1['group'] == '13'].head(20)
+markers1.loc[markers1['group'] == '14'].head(20)
+
+
+Cell_class = { 
+"0": "Neuron",
+"1": "Neuron", # Excitatory
+"2": "Oligodendrocyte", 
+"3": "Astrocyte", #Apoe+
+"4": "Macrophage",  # mt-Co1, Apoe, Ptgds, Hsp90ab1, Cst3, Rpl13
+"5": "Neuron", 
+"6": "Ptgds+", # 
+"7": "OPC",
+"8": "Neuron", 
+"9": "Vascular",
+"10": "Neuron",
+"11": "Neuron", 
+"12": "Neuron",
+"13": "Neuron",
+"14": "Nnat+"
+}
+
+adata1.obs['Cell_class'] = adata1.obs.leiden.map(Cell_class)
+
+
+sc.pl.umap(adata1, color = ['Cell_class'], legend_loc = 'on data', legend_fontsize = 10)
+sc.pl.umap(adata1, color = ['leiden'], legend_loc = 'on data', legend_fontsize = 20)
+
+
+
+################################################################################################################################
+
+# KO - MARKERS
+
+sc.tl.rank_genes_groups(adata2, groupby='leiden', use_raw = True, method = 'wilcoxon')
+sc.pl.rank_genes_groups(adata2, n_genes=30)
+
+sc.pl.umap(adata2, color = ['Rbfox3', 'Gad1', 'Slc17a6', 'Snap25',
+                            'Mbp', 'Plp1', 'Sox10', 'Pdgfra', 'Ptprz1',
+                            'Slc1a2', 'Apoe', 'Atp1a2', 'Aqp4', 'Aldh1l1', 'Elmo1',
+                            'Hexb', 'Tgfbr1', 'C1qa',
+                            'Cldn5', 'Flt1', 'Vtn',
+                            'mt-Atp6', 'mt-Co1', 'Rpl13', 'Ptgds', 'Hsp90ab1', 
+                            'Pecam1', 'Rgs5'])
+
+sc.pl.umap(adata2, color = 'leiden')
+
+markers2 = sc.get.rank_genes_groups_df(adata2, group = None)
+markers2 = markers2[(markers2.pvals_adj < 0.05) & (markers2.logfoldchanges > .5)]
+markers2
+
+
+markers2.loc[markers2['group'] == '0'].head(20)
+markers2.loc[markers2['group'] == '1'].head(20)
+markers2.loc[markers2['group'] == '2'].head(20)
+markers2.loc[markers2['group'] == '3'].head(20)
+markers2.loc[markers2['group'] == '4'].head(20)
+markers2.loc[markers2['group'] == '5'].head(20)
+markers2.loc[markers2['group'] == '6'].head(30)
+markers2.loc[markers2['group'] == '7'].head(20)
+markers2.loc[markers2['group'] == '8'].head(20)
+markers2.loc[markers2['group'] == '9'].head(20)
+markers2.loc[markers2['group'] == '10'].head(30)
+markers2.loc[markers2['group'] == '11'].head(20)
+markers2.loc[markers2['group'] == '12'].head(20)
+markers2.loc[markers2['group'] == '13'].head(20)
+markers2.loc[markers2['group'] == '14'].head(20)
+markers2.loc[markers2['group'] == '15'].head(20)
+markers2.loc[markers2['group'] == '16'].head(20)
+markers2.loc[markers2['group'] == '17'].head(20)
+markers2.loc[markers2['group'] == '18'].head(20)
+markers2.loc[markers2['group'] == '19'].head(20)
+markers2.loc[markers2['group'] == '20'].head(20)
+markers2.loc[markers2['group'] == '21'].head(20)
+
+
+
+Cell_class = { 
+"0": "Mito-high",
+"1": "Oligodendrocytes",
+"2": "Neuron", 
+"3": "Astrocyte", #Apoe+
+"4": "Neuron",
+"5": "Neuron", 
+"6": "Neuron", # 
+"7": "",
+"8": "", 
+"9": "Macrophage",
+"10": "",
+"11": "", 
+"12": "Neuron",
+"13": "",
+"14": "",
+"15": "Vascular",
+"16": "",
+"17": "Neuron",
+"18": "Neuron",
+"19": "Astrocyte",
+"20": "Vascular",
+"21": "Neuron"
+}
+
+adata1.obs['Cell_class'] = adata1.obs.leiden.map(Cell_class)
+
+
+sc.pl.umap(adata2, color = ['Cell_class'], legend_loc = 'on data', legend_fontsize = 10)
+sc.pl.umap(adata2, color = ['leiden'], legend_loc = 'on data', legend_fontsize = 20)
+
+
+################################################################################################################################
+
+# Neurons
+sc.pl.umap(adata1, color = ['Rbfox3', 'Snap25', 'Gad1', 'Gad2', 'Slc17a6', 'Slc17a7'], ncols = 3)
+sc.pl.umap(adata2, color = ['Rbfox3', 'Snap25', 'Gad1', 'Gad2', 'Slc17a6', 'Slc17a7'], ncols = 3)
+
+# Macrophages
+sc.pl.umap(adata1, color = ['Hexb', 'P2ry12', 'Tgfbr1', 'Itgam', 'Itgax', 'Mpeg1', 'Cx3cr1', 'Mrc1', 'H2-Ab1'], ncols = 3)
+sc.pl.umap(adata2, color = ['Hexb', 'P2ry12', 'Tgfbr1', 'Itgam', 'Itgax', 'Mpeg1', 'Cx3cr1', 'Mrc1', 'H2-Ab1'], ncols = 3)
+
+# Astrocytes
+sc.pl.umap(adata1, color = ['Gfap', 'Aldh1l1', 'Aqp4', 'Slc1a2', 'Atp1a2', 'Apoe'], ncols = 3)
+sc.pl.umap(adata2, color = ['Gfap', 'Aldh1l1', 'Aqp4', 'Slc1a2', 'Atp1a2', 'Apoe'], ncols = 3)
+
+# Oligodendrocytes
+sc.pl.umap(adata1, color = ['Olig1','Olig2', 'Sox10', 'Pdgfra', 'Mbp', 'Mog'], ncols = 3)
+sc.pl.umap(adata2, color = ['Olig1','Olig2', 'Sox10', 'Pdgfra', 'Mbp', 'Mog'], ncols = 3)
+
+# Vascular
+sc.pl.umap(adata1, color = ['Cldn5','Cdh5', 'Flt1', 'Pecam1', 'Icam1', 'Rgs5'], ncols = 3)
+sc.pl.umap(adata2, color = ['Cldn5','Cdh5', 'Flt1', 'Pecam1', 'Icam1', 'Rgs5'], ncols = 3)
+
+
+# Cell stress genes
+adata1.var.mt[adata1.var.mt].index
+adata2.var.mt[gdata.var.mt].index
+
+sc.pl.umap(adata1, color = ['mt-Co3', 'mt-Atp6', 'mt-Co2', 'mt-Nd2', 'Rpl13', 'Rplp1', 'Ptgds', 'Hsp90ab1', 'Sod1'], ncols = 3)
+sc.pl.umap(adata2, color = ['mt-Co3', 'mt-Atp6', 'mt-Co2', 'mt-Nd2', 'Rpl13', 'Rplp1', 'Ptgds', 'Hsp90ab1', 'Sod1'], ncols = 3)
+
+################################################################################################################################
+
+
+
+## Export annotations
+
+dir = "/Users/maureen/Documents/projects/lukens-lab/jess/Thanos_AD-Sting_snRNAseq/mc-analysis/h5ad"
+os.chdir(dir)
+os.getcwd()
+
+sc.write('sting-globalWT.h5ad', adata1)
+sc.write('sting-globalKO.h5ad', adata2)
+
+
+################################################################################################################################
+
 
 
 ################################################################################################################################
 
 # CONCATENATION
 
-adata = gdata1.concatenate(gdata2, batch_key='genotype', batch_categories=['WT', 'KO'] )
+adata = adata1.concatenate(adata2, batch_key='genotype', batch_categories=['WT', 'KO'] )
 adata.obs # concatenation adds suffix
 
 ################################################################################################################################
+
+# PCA AND CLUSTERING
+
+sc.tl.pca(adata)
+sc.pl.pca_variance_ratio(adata, log=True, n_pcs=50)
+sc.pp.neighbors(adata, n_neighbors=10, n_pcs=50)
+sc.tl.umap(adata)
+sc.tl.leiden(adata, resolution=0.5)
+
+## Visualize results
+sc.pl.umap(adata, color = ['genotype', 'leiden'])
+
+################################################################################################################################
+
+# RUN HARMONY 
+
+#sce.pp.harmony_integrate(adata, 'genotype')
+#adata.obsm['X_pca'] = adata.obsm['X_pca_harmony']
+#sc.pp.neighbors(adata, n_neighbors=10, n_pcs=30)
+#sc.tl.umap(adata)
+#sc.tl.leiden(adata, resolution=0.5)
+
+## Visualize results
+#sc.pl.umap(adata, color = ['genotype', 'leiden'])
 
 ################################################################################################################################
